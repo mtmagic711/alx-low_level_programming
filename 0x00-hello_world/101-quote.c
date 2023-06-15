ac6ccac518527ac56bd213061a3e20c160e8a82f
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-15 07:24:27 -0700

    A lot of fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quot, sizeof(quot));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+ sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
