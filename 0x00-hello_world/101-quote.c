fc3415d7811bc2d106b6b720e99cf253053b938f
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-15 06:53:22 -0700

    A quote without usuall output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Print a quote without printf and puts
+ *
+ * Return: The value 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write (2, quote, sizeof(quote));
+	return (1);
+}
