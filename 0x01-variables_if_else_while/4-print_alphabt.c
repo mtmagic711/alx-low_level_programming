5f468d159ffb55bd00c2a3b8eb94a7ebbd97910c
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-17 07:44:52 -0700

    Alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - alphabet without q and e
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char lett = 'a';
+
+while(lett <= 'z')
+{
+if (lett != 'e' && lett != 'q')
+{
+putchar(lett);
+}
+lett++;
+}
+putchar('\n');
+return (0);
+}
