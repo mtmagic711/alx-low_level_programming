b99c930ddb15289b16d4f22bddec40de9dd960cf
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-17 08:21:09 -0700

    print numbers from Ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Print digits without printf
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
