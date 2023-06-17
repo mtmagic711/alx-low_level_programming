2e22d0cb7089beb73ef77184659eda581b649ce2
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-17 10:07:05 -0700

    cobination of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - print a combination of two numbers
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int i, j;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
