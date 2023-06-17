079435a27a0b78bf8919362147abaf7d26357844
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-17 10:25:57 -0700

    a three combinations of numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print a three combination 
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int i, j, z;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+for (z = j + 1; z <= 57; z++)
+{
+putchar(i);
+putchar(j);
+putchar(z);
+if (i == 55 && j == 56 && z == 57)
+{
+break ;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
