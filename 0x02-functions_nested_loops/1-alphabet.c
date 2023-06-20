a9225677c6b5200884dfd14c43195dde476ec083
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-20 10:21:07 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - print alphabet
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char alph = 'a';
+
+while (alph <= 'z')
+{
+_putchar(alph);
+alph++;
+}
+_putchar('\n');
+return (0);
+}
