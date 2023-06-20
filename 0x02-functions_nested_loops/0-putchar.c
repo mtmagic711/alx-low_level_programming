bcaebd4b6ef5e712712edb30984af2af7dea6547
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-20 10:03:45 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,13 @@
 */
 int main(void)
 {
+char *tab = "_putchar";
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');	
-	_putchar('\n');
-	return (0);
+while (*tab)
+{
+_putchar(*tab);
+tab++;
+}
+_putchar('\n');
+return (0);
 }
