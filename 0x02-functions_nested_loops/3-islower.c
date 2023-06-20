87035e830d4b32999e4a2a85809d1c5ceb692198
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-20 14:22:24 -0700

    c is lowercase char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+ /**
+* _islower - to verify a character is lower or not
+* @c: the value that the fonction
+* Return: 0 or 1
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+	return (1);
+else
+	return (0);
+}
+
