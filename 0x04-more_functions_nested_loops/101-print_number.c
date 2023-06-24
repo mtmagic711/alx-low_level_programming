de5ab4ff32d18872af0e6e80872f002857f1120f
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-23 19:30:56 -0700

    print an integar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_number - print integers.
+* @n: the intger to print.
+* Return: Always 0.
+*/
+
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+}
