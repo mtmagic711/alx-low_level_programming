b1d1068b1b5144b4a133e27a7024009a69ca6f4e
Abderrahmane <mohamedhamyd5@gmail.com>
2023-06-29 18:03:17 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,20 @@ char *cap_string(char *str)
 {
 int i = 0;
 
-if (*(str + i) >= 'a' && *(str + i) <= 'z')
+if (str[i] >= 'a' && str[i] <= 'z')
 {
-	*(str + i) = *(str + i) - 32;
+	str[i] -= 32;
 }
-while (*str + i)
+while (str[i])
 {
-	if (*(str + i) == ' ' || *(str + i) == '\t' || *(str + i) == '\n' ||
-		*(str + i) == ',' || *(str + i) == ';' || *(str + i) == '.' ||
-		*(str + i) == '!' || *(str + i) == '?' || *(str + i) == '"' ||
-		*(str + i) == '(' || *(str + i) == ')' || *(str + i) == '{' ||
-		*(str + i) == '}')
+	if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ','
+		|| str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?'
+		|| str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '{'
+		|| str[i] == '}')
 	{
-		if (*(str + i + 1) >= 'a' && *(str + i + 1) <= 'z')
+		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 		{
-			*(str + i + 1) -= 'a' - 'A';
+			str[i + 1] -= 'a' - 'A';
 		}
 	}
 	i++;
