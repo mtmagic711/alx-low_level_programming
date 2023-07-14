e5bba28713e8b302863df6d07d96d6d5788db3ba
Abderrahmane <mohamedhamyd5@gmail.com>
2023-07-13 18:48:26 -0700

    numbers in order

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* array_range - range numbers.
+* @min: the smallest nm.
+* @max: the largest nm.
+* Return: the numbers in order.
+*/
+
+int *array_range(int min, int max)
+{
+int *array;
+int ln;
+int i = 0;
+
+if (min > max)
+	return (NULL);
+array = malloc(ln * sizeof(int));
+if (array == NULL)
+	return (NULL);
+while (array[i] < ln)
+{
+	array[i] = (min + i);
+}
+return (array);
+}
