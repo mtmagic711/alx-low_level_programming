b20ed0984ac23298eaef05d28b55dba1e3f02a0b
Abderrahmane <mohamedhamyd5@gmail.com>
2023-07-27 19:39:35 -0700

    print before

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_before - print a sentence.
+*
+* Return: Always 0.
+*/
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
