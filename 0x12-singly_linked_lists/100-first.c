5b864cb3c8cd8064b30fd92bceadf9805e36f193
Abderrahmane <mohamedhamyd5@gmail.com>
2023-07-27 19:51:01 -0700

    print before

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 
+void print_before(void) __attribute__ ((constructor));
+
 /**
 * print_before - print a sentence.
 *
 * Return: Always 0.
 */
 
-void print_before(void) __attribute__ ((constructor));
 void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
