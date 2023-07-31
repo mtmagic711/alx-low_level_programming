258368bec427dba9f52fcc0525c4be2bf79f9730
mtmagic <mtmagic711@gmail.com>
2023-07-31 14:02:36 -0700

    free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_listint2 - frees a list.
+* @head: the head of the list.
+* Return: Always 0.
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+}
