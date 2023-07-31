1059725176acb8c93a80b41a6e55204db24d32e4
mtmagic <mtmagic711@gmail.com>
2023-07-31 11:57:47 -0700

    number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* listint_len - returns the number of elements
+* @h: the head.
+* Return: returns the number of elements.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t cmpt = 0;
+
+	while (h != NULL)
+	{
+		cmp++;
+		h = h->next;
+	}
+	return (cmpt);
+}
