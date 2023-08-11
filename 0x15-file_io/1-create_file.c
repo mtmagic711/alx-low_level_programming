32b1d3052a6b57995e0b9e583f3deb7e36af612f
Abderrahmane <mohamedhamyd5@gmail.com>
2023-08-12 00:02:50 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (*(text_content + i) != '\0')
+	if (text_content == NULL)
+		i = 0;
+	else
+	{
+		while (*(text_content + i) != '\0')
 		i++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
