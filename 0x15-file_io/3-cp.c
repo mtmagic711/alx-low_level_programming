b8955fa52b101a0a84abad1fc1a08559db13760c
Abderrahmane <mohamedhamyd5@gmail.com>
2023-08-12 01:30:36 +0100

    cp from to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,12 +41,14 @@ int main(int argc, char *argv[])
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
 		exit(99);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
 		exit(98);
 	}
 	re = read(fd, buffer, 1024);
