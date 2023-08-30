194532be0fbd25e470bb344407f841251d5559ba
Abderrahmane <mohamedhamyd5@gmail.com>
2023-08-30 22:19:16 +0100

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	_putchar(*(s + i));
-	i++;
 	if (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		i++;	
 		_puts_recursion(s + i);
+	}
 	else
 		_putchar('\n');
 }
