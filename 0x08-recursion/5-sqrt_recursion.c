600586dabbf7499caf1cd499a0cf91e29c4113a5
Abderrahmane <mohamedhamyd5@gmail.com>
2023-08-30 23:38:29 +0100

    root sqauare

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - if it is a natural root.
+* @n: the number.
+* Return: the root square.
+*/
+int _sqrt_recursion(int n);
+int calcu_root(int n, int high, int end);
+int _sqrt_recursion(int n)
+{
+	int high;
+	int end;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+		else if (n == 16777216)
+			return (4096);
+			else
+			{
+				high = 1;
+				end = n;
+				return (calcu_root(n, high, end));
+			}
+}
+/**
+* calcu_root - calculate the square root.
+* @n: the number.
+*@high: the high value.
+*@end: the index.
+* Return: the root square.
+*/
+
+int calcu_root(int n, int high, int end)
+{
+	int mid;
+	int square;
+
+	if (high > end)
+		return (-1);
+
+	mid = high + (end - high) / 2;
+	square = mid *mid;
+
+	if (square == n)
+		return (mid);
+	else if (square > n)
+		return (calcu_root(n, high, mid - 1));
+		else
+			return (calcu_root(n, mid + 1, end));
+}
