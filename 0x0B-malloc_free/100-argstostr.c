5ba2e69fb9d59871b0075e2830991d2f58771698
Abderrahmane <mohamedhamyd5@gmail.com>
2023-09-06 02:20:13 +0100

    arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * argstostr - concatenates all the arguments.
@@ -9,7 +10,34 @@
 
 char *argstostr(int ac, char **av)
 {
+	int total_length = 0;
+	char *result;
+	int i, j;
+	int index = 0;
 
-	(void)ac;
-	(void)av;
+	if (ac == 0 || av == NULL)
+	        return NULL;
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+			total_length += strlen(av[i]) + 1;
+	}
+
+	result = (char *)malloc(sizeof(char) * (total_length + 1));
+	if (result == NULL)
+		return NULL;
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
+         		for (j = 0; av[i][j] != '\0'; j++)
+         			result[index++] = av[i][j];
+		result[index++] = '\n';
+		}
+	}
+	
+	result[index] = '\0';
+	return result;
 }
