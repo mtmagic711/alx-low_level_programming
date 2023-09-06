40c391d4da3e812db99bcbc5658701dee4c24463
Abderrahmane <mohamedhamyd5@gmail.com>
2023-09-06 02:04:00 +0100

    arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* argstostr - concatenates all the arguments.
+* @ac: number of arguments.
+* @av: the commands.
+* Return: a string.
+*/
+
+char *argstostr(int ac, char **av)
+{
+
+}
