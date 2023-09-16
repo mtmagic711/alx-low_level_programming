28c535af88f4a276102ed6119b22d0de84d4e71a
mtmagic <mtmagic711@gmail.com>
2023-09-16 03:53:24 +0100

    p

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+* main - do the maths.
+* @argc: number of arguments.
+* @argv: an array of strings.
+* Return: Always 0.
+*/
+if (argc == 4)
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*f)(int, int);
+
+	if (argc == 4)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		f = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+	
+		printf("Error\n");
+		exit(100);
+	}
+}
+	printf("%d\n", f(a, b));
+	
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the 1st integer
+ * @b: the 2nd integer
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substructs two integers
+ * @a: the 1st integer
+ * @b: the 2nd integer
+ * Return: the difference.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: the 1st integer
+ * @b: the 2nd integer
+ * Return: the result of a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - computes a / b
+ * @a: the 1st integer
+ * @b: the 2nd integer
+ * Return: the result of a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - computes a % b
+ * @a: the 1st integer
+ * @b: the 2nd integer
+ * Return: the result of a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
