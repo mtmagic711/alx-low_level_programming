6ef0ad1c523a2a8411797c3c0eee25d2573287b1
mtmagic <mohamedhamyd5@gmail.com>
2023-09-22 00:17:13 +0100

    print all

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* print_list -  prints all the elements of a list_t list.
+* @h: the list.
+* Return: the number of elements.
+*/
+
+size_t print_list(const list_t *h)
+{
+	list_t *temp;
+	size_t cmp = 0;
+
+	*temp = h;
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", temp->len, temp->str);
+	cmpt++;
+	temp = temp->next;
+	}
+	return(cmp);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Starting with singly linked list.
