b1cf9804a7db21d0f0bae7204855f950fc768352
mtmagic <mohamedhamyd5@gmail.com>
2023-09-27 00:02:54 +0100

    insert at n position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				temp->next = node;
 				return (node);
 			}
+			if (temp == NULL)
+			{
+				free(node);
+				return (NULL);
+			}
 			temp = temp->next;
 			i++;
 		}
