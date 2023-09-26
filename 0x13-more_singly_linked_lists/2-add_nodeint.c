97630b0431ddea4ecf98373d9f7994d4b3e4db3f
mtmagic <mohamedhamyd5@gmail.com>
2023-09-26 18:57:03 +0100

    at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
 * add_nodeint - adds a new node at the beginning of a list.
-* @h: the head of a the list
+* @head: the head of a the list
 * @n: the data to add.
 * Return: the address of the new element, or NULL if it failed.
 */
