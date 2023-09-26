6fa9d5927415f46d9472952694e592d4d8a9a5dc
mtmagic <mohamedhamyd5@gmail.com>
2023-09-26 18:39:31 +0100

    redoing it

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++
@@ -1,9 +0,0 @@
-#include "lists.h"
-
-/**
-* listint_len - the number of elements in a list.
-* @h: the head of a the list
-* Return: number of elements.
-*/
-
-size_t listint_len(const listint_t *h)
