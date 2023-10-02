ea69d9a9d8a0acc447896312c6ba3a3c7b580eea
mtmagic <mohamedhamyd5@gmail.com>
2023-10-03 00:53:17 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,11 +25,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	wr = write(fd, text_content, len);
+	wr = write(fd, text_content - len, len);
 	if (wr == -1)
+	{
+		close(fd);
 		return (-1);
-	close(fd);
-	if (fd == -1)
+	}
+	if (close(fd) == -1)
 		return (-1);
 
 	return (1);
