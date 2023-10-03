01d263a20ce1a02b87522846a6cd16768e061a20
mtmagic <mohamedhamyd5@gmail.com>
2023-10-03 01:14:28 +0100

    append to

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*
-*
-*
-*
+* append_text_to_file - appends text at the end of a file.
+* @filename: the file name.
+* @text_content: the content to append to the file.
+* Return: 1 on success and -1 on failure.
 */
 
 int append_text_to_file(const char *filename, char *text_content)
