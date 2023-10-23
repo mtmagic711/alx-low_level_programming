8946789994e22d2829c135156b143b8d91b5ce85
mtmagic <mohamedhamyd@gmail.com>
2023-10-23 23:33:24 +0100

    deffering

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _memset -  fills memory with a constant byte.
+* @s: pointer to the memory area.
+* @b: the constant byte.
+* @n: the number of byte to fill.
+* Return: the pointer to s.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *pt;
+pt = s;
+
+while (n > 0)
+{
+	*pt = b;
+	pt++;
+	n--;
+}
+return (s);
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* *_strcat - append a string to another.
+* @dest: the string to append to.
+* @src: the string to add.
+* Return: A pointer to dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = j = 0;
+while (*(dest + i) != '\0')
+{
+	i++;
+}
+
+*(dest + i) = ' ';
+while (*(src + j))
+{
+	*(dest + i) = *(src + j);
+	i++;
+	j++;
+}
+*(dest + i) = '\0';
+return (dest);
+}
