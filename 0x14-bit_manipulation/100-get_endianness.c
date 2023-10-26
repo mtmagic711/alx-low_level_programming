24b476e34bbe9c3ab9239b4571433ceb46a8e074
mtmagic <mohamedhamyd@gmail.com>
2023-10-27 00:37:34 +0100

    big indian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,12 @@
+/**
+* get_endianness - function that checks the endianness.
+*
+* Return: 0 if big endian, 1 if little endian
+*/
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	unsigned char* bytePtr = (unsigned char*)&num;
+	unsigned char *bytePtr = (unsigned char *)&num;
 
-	return (bytePtr[0] == 1) ? 1 : 0;
+	return ((bytePtr[0] == 1) ? 1 : 0);
 }
