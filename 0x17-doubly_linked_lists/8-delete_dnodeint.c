0ba38d047b1e39572c3e5c643d53a049c4776bfd
mtmagic711 <mtmagic711@gmail.com>
2023-12-08 10:35:58 +0100

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,3 +8,36 @@
 */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr = *head;
+
+	if (!curr)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = curr->next;
+		if (curr->next)
+			curr->next->prev = NULL;
+		free(curr);
+		return (1);
+	}
+
+	while (index > 0 && curr)
+	{
+		curr = curr->next;
+		index--;
+	}
+
+	if (!curr)
+		return (-1);
+
+	if (curr->prev)
+		curr->prev->next = curr->next;
+
+	if (curr->next)
+		curr->next->prev = curr->prev;
+
+	free(curr);
+	return (1);
+}
