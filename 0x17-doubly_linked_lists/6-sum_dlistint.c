24f7a28bad13e8c8d211a315efbc5bfd80f5f7d4
mtmagic711 <mtmagic711@gmail.com>
2023-12-08 10:31:11 +0100

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,3 +7,16 @@
 */
 
 int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *node = head;
+	int sum = 0;
+
+	if (node == NULL)
+		return (0);
+	while (node != NULL)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+	return (sum);
+}
