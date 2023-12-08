1cbe434140ca2180489cc025d6715a74a4af6413
mtmagic711 <mtmagic711@gmail.com>
2023-12-08 10:37:30 +0100

    task 8

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,5 +29,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 size_t dlistint_len(const dlistint_t *h);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /*LISTS_H*/
