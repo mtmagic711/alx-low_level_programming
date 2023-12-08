5038a6af7fd806ed2d27c5112892374f92a553cd
mtmagic711 <mtmagic711@gmail.com>
2023-12-08 09:57:20 +0100

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,4 +7,22 @@
 * Return: the address of the new element, or NULL if it failed.
 */
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current;
+
+	current = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (current == NULL)
+		return (NULL);
+	current->next = NULL;
+	current->prev = NULL;
+	current->n = n;
+	if (head == NULL)
+	{
+		*head = current;
+		return (current);
+	}
+	current->next = *head;
+	*head = current;
+	return (current);
+}
