8d65cbbdccbaad6fdaf45b856ad890a62c1f7a3e
mtmagic711 <mtmagic711@gmail.com>
2023-12-08 09:18:41 +0100

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,3 +7,17 @@
 */
 
 size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+	current = h;
+	while (current)
+	{
+		num++;
+		current = current->next;
+	}
+	return (num);
+}
